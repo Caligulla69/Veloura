f5c8694e4c71dc3af62490ec1f0122e7a2ce892f
Shoaib Ali <chadshoaib1869@gnail.com>
2025-10-18 00:47:50 +0500

    changed server.js added samesite

--- Backend/server.js
+++ Backend/server.js
@@ -51,7 +51,8 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      secure: false, // Use secure cookies in production
+      sameSite: "none",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
