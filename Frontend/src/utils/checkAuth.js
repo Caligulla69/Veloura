4f5110718a74343c877cb575047b363fc091b149
Shoaib Ali <chadshoaib1869@gnail.com>
2025-10-18 00:34:55 +0500

    added get on checkAuth

--- Frontend/src/utils/checkAuth.js
+++ Frontend/src/utils/checkAuth.js
@@ -1,7 +1,7 @@
 import API_URL from "./api";
 
 export const checkAuth = async () => {
-  const res = await fetch(`${API_URL}/checkAuth`, { credentials: "include" });
+  const res = await fetch(`${API_URL}/checkAuth`, { credentials: "include",method:"GET" });
   const data = await res.json();
   console.log(data.isLoggedIn);
   
